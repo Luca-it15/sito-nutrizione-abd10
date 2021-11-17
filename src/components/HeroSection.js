90d9466c9bcad2920305a81ecc1d1429b13031c0
Stackbit Code Editor <projects@stackbit.com>
2021-11-17 11:55:36 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -14,7 +14,7 @@ export default class HeroSection extends React.Component {
         let bg_img_opacity_pct = _.get(section, 'background_image_opacity', null) || 100;
         let bg_img_opacity = bg_img_opacity_pct * 0.01;
         let bg_img_size = _.get(section, 'background_image_size', null) || 'cover';
-        let bg_img_position = _.get(section, 'background_image_position', "#7ED957") || 'center center';
+        let bg_img_position = _.get(section, 'background_image_position', null) || 'center center';
         let bg_img_repeat = _.get(section, 'background_image_repeat', null) || 'no-repeat';
         let has_text = false;
         let has_media = false;
@@ -39,7 +39,7 @@ export default class HeroSection extends React.Component {
         }
         return (
             <React.Fragment>
-                <section className={classNames('section', 'hero', {'has-border': _.get(section, 'has_border', null), 'has-cover': _.get(section, 'background_image', null), 'bg-none': bg_color === 'none', 'bg-primary': bg_color === '#7ED957', 'bg-secondary': bg_color === 'secondary', 'pt-4': padding_top === 'small', 'pt-6': (padding_top === 'medium') || (padding_top === 'large'), 'pt-md-7': padding_top === 'large', 'pb-4': padding_bottom === 'small', 'pb-6': (padding_bottom === 'medium') || (padding_bottom === 'large'), 'pb-md-7': padding_bottom === 'large'})}>
+                <section className={classNames('section', 'hero', {'has-border': _.get(section, 'has_border', null), 'has-cover': _.get(section, 'background_image', null), 'bg-none': bg_color === 'none', 'bg-primary': bg_color === 'primary', 'bg-secondary': bg_color === 'secondary', 'pt-4': padding_top === 'small', 'pt-6': (padding_top === 'medium') || (padding_top === 'large'), 'pt-md-7': padding_top === 'large', 'pb-4': padding_bottom === 'small', 'pb-6': (padding_bottom === 'medium') || (padding_bottom === 'large'), 'pb-md-7': padding_bottom === 'large'})}>
                 	{_.get(section, 'background_image', null) && (
                 	<div className="cover-img" style={toStyleObj('background-image: url(\'' + withPrefix(_.get(section, 'background_image', null)) + '\'); opacity: ' + bg_img_opacity + '; background-size: ' + bg_img_size + '; background-repeat: ' + bg_img_repeat + '; background-position: ' + bg_img_position)}/>
                 	)}
