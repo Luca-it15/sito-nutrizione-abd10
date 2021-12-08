b0cb19347745ba17d5754c22f49db809ba8fb013
Stackbit Code Editor <projects@stackbit.com>
2021-12-08 14:54:34 +0000

    page.js: changed

--- src/layouts/page.js
+++ src/layouts/page.js
@@ -21,6 +21,7 @@ export default class Page extends React.Component {
                     </h4>
                     <h5>Toscana Capannori (Lucca)</h5>
                     <h6>Via Pesciatina, Lunata 60</h6>
+                    
             	</div>
             </article>
             </Layout>
