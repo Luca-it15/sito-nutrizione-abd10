54f53470ba4390ada08d3ed3bcd531f0e8c8057f
Stackbit Code Editor <projects@stackbit.com>
2021-12-08 13:47:47 +0000

    page.js: changed

--- src/layouts/page.js
+++ src/layouts/page.js
@@ -17,7 +17,7 @@ export default class Page extends React.Component {
                         {markdownify(_.get(this.props, 'page.content', null))}
                         
             		</div>
-                    <a className="navbar__logo m-0" href={"https//www.google.com/maps/place/Via+dell'+Isola,+16,+55012+Capannori+LU/@43.8548052,10.5591787,17z/data=!3m1!4b1!4m5!3m4!1s0x12d58117b9862b25:0x91bda9cbf1454f5a!8m2!3d43.8548052!4d10.5613674"}>
+                    <a  href={"https//www.google.com/maps/place/Via+dell'+Isola,+16,+55012+Capannori+LU/@43.8548052,10.5591787,17z/data=!3m1!4b1!4m5!3m4!1s0x12d58117b9862b25:0x91bda9cbf1454f5a!8m2!3d43.8548052!4d10.5613674"}>
                             <img src={"../../images/personal-preview.png"} alt={_.get(this.props, 'data.config.header.logo_alt', null)} />
                         </a>
             	</div>
