0461b3472825aa674984b7cde11130cba3acc10f
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 15:09:01 +0000

    page.js: changed

--- src/layouts/page.js
+++ src/layouts/page.js
@@ -11,7 +11,7 @@ export default class Page extends React.Component {
             <article className="page py-5 py-sm-6">
             	<div className="container container--medium">
             		<header className="page__header">
-            			<h1 className="page__title">{_.get(this.props, 'page.title', null)}</h1>
+            			<h1 style="text-alignclassName="page__title">{_.get(this.props, 'page.title', null)}</h1>
             		</header>
             		<div className="page__body text-block">
                         {markdownify(_.get(this.props, 'page.content', null))} 
