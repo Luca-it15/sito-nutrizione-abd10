e3791df9b32b5e3b4b01e03767261ea423ca2690
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 14:26:24 +0000

    page.js: changed

--- src/layouts/page.js
+++ src/layouts/page.js
@@ -32,7 +32,7 @@ export default class Page extends React.Component {
                       STUDI DI PSICOTERAPIA E NUTRIZIONE 
                     </h4>
                     <h5> Fucecchio (Firenze)</h5>
-                    <h6>Via Pesciatina, Lunata 60</h6>
+                    <h6>Via Giotto 13</h6>
                   <a href="">
                       <img src="../../images/studimediciSanLuca.png" alt="st
                    </p>
